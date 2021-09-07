6dfa00b22db20d94f49ad11dfa126dfcffaade98
Sreeram Shenai <sreeramshenai2000@gmail.com>
2021-09-08 02:03:56 +0530

    add file app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var txtarea = document.querySelector("#txtarea");
+var button = document.querySelector("#buttonArea");
+var txt = txtarea.value;
+buttonArea.addEventListener("click",function() {
+    var txt = txtarea.value;
+    console.log("input "+ txt);
+})
