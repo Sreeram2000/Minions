6f2fb91c6c2cc79ffb9a7f5addf41975b5e6f0df
Sreeram Shenai <sreeramshenai2000@gmail.com>
2021-09-08 19:53:47 +0530

    edit

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var button = document.querySelector("#buttonArea");
 var out = document.querySelector("#outarea")
 
 button.addEventListener("click",function() {
-    var txt = txtarea.value;
-    var url = "https://api.funtranslations.com/translate/minion.json" + "?text=" + txt;
+    var url = "https://api.funtranslations.com/translate/minion.json" + "?text=" + txtarea.value;
     fetch(url)
         .then(response => response.json())
         .then(json => {
